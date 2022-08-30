828fadba3faf3cee6afa686d0c17a7468013e69a
OtajonMakxmasoliyev <otajonmakxmasoliyev3112@gmail.com>
2022-08-31 00:17:56 +0500

    Xaridor coinlari qismi tugallandi

--- src/components/linechart.jsx
+++ src/components/linechart.jsx
@@ -12,6 +12,7 @@ import {
     Filler
 } from 'chart.js';
 import Code from "./code.json"
+import { borderRadius } from "@mui/system";
 
 
 ChartJS.register(
@@ -29,26 +30,17 @@ export default class LineChart extends React.Component {
         super(props);
         this.state = {
             data: {
-                labels: Code.data.coins.map(x => x.name),
+                labels: Code.data.coins.map((x) => x.price),
                 // labels: [],
                 datasets: [
                     {
-                        label: "Rank",
+                        label: Code.data.stats.totalCoins,
                         backgroundColor: "red",
-                        data: Code.data.coins.map(x => x.sparkline),
-                        borderColor: "red",
+                        data: Code.data.stats.total24hVolume,
+                        borderColor: "#15A9FE",
                         fill: true,
                         tension: 0.3
 
-                    },
-
-                    {
-                        label: "Solo",
-                        backgroundColor: "green",
-                        data: Code.data.coins.map(x => x.btcPrice),
-                        borderColor: "red",
-                        fill: true,
-                        tension: 0.5
                     }
                 ]
             }
@@ -60,25 +52,32 @@ export default class LineChart extends React.Component {
         function grd(color) {
             const c = document.createElement("canvas")
             var ctx = c.getContext("2d");
-            const grd = ctx.createLinearGradient(0, 0, 1000, 1500);
-            grd.addColorStop(0, color);
-            grd.addColorStop(0.5, "white");
-            grd.addColorStop(1, color);
+            const grd = ctx.createLinearGradient(0, -15, 150, 1500);
+            // const grd = ctx.createRadialGradient(0, 15, 0, 15 1000, 0);
+            grd.addColorStop(0, "#15A9fe");
+            grd.addColorStop(0.5, "transparent");
+            grd.addColorStop(0.25, "#000000");
+            grd.addColorStop(0.2, "transparent");
             ctx.fillStyle = grd;
-            ctx.fillRect(10, 10, 150, 80);
+            ctx.fillRect(10, 10, 135, 80);
             return grd;
         }
 
-        let colors = ["rgba(255, 0, 255, 0.75)", "rgba(0, 0, 255 ,0.75)"];
+        let colors = ["#1573FE", "#1573FE"];
 
         const data = this.state.data
-        if (data.datasets) {
-            data.datasets.forEach((set, i) => {
-                set.backgroundColor = grd(colors[i]);
-                console.log(grd(colors[i]));
-                // set.backgroundColor = colors[i]
-            })
-        }
+        // if (data.datasets) {
+        //     data.datasets.forEach((set, i) => {
+        //         set.backgroundColor = grd(colors[i]);
+        //         console.log(typeof colors[i]);
+        //         // set.backgroundColor = colors[i]
+        //     })
+        // }
+        data.datasets.map(x => {
+            x.backgroundColor = grd("#1573FE")
+
+        })
+
 
         return data;
 
@@ -90,14 +89,31 @@ export default class LineChart extends React.Component {
 
     render() {
 
-        for (let i = 0; i < 2; i++) {
-            console.log(this.state.data.datasets[i].backgroundColor);
+        // for (let i = 0; i < 2; i++) {
+        //     console.log(this.state.data.datasets[i].backgroundColor);
 
-        }
+        // }
         return (
-            <div style={{ width: 1000, height: 1500 }}>
-                <h3>Sample</h3>
-                <Line ref={this.colorPickerRef} options={{ responsive: true }} data={this.componentDidMount()} />
+            <div style={{ width: "150%", height: "53%", boxSizing: "border-box", overflow: "hidden", }} >
+                <Line width={"130%"} height={50} ref={this.colorPickerRef} options={{
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    },
+                    scales: {
+                        x: {
+                            ticks: false
+                        },
+                        y: {
+                            beginAtZero: true,
+                            display: false,
+                            drawBorder: true
+                        }
+
+                    }
+                }} data={this.componentDidMount()} />
             </div>
         )
     }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import Navbar from '../components/Navbar';
+import LineChart from '../components/linechart';
+import CodeJson from "../components/code.json"
 
-const Home = () => {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+
+const Code = CodeJson;
+
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            yourCoin: "",
+            startYourCoin: "",
+        }
+    }
+
+    render() {
+        const data = Code;
+        this.state.yourCoin = parseFloat(data.data.coins[1].price).toFixed(3)
+        this.state.startYourCoin = (parseFloat(data.data.coins[1].btcPrice).toFixed(6) * 100).toFixed(4)
+        let Percent = parseFloat(this.state.yourCoin) / (parseFloat(this.state.yourCoin) / 100) * 2
+        Percent = Percent.toString()
+        return (
+            <div className='home' >
+                <Navbar />
+                <div className='mainBlock'>
+                    <div className="assets">
+                        <h3>Sample</h3>
+                        <p style={{ color: "white " }} className="balance">Your total balance</p>
+                        <div className="total">
+                            <span style={{ color: "white" }} className="youcoin">$ {this.state.yourCoin}</span>
+                            <span style={{ color: "white" }} className="startcoin">$ {this.state.startYourCoin}</span>
+                            <span className="percent"><ArrowDropUpIcon /> {Percent}% </span>
+                        </div>
+
+
+                        <LineChart />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Home
